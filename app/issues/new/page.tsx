9498035d089612a0b652594aea11a660df28a54e
agkayster <ejike.chiboka@gmail.com>
2023-11-28 10:55:04 +0100

    Handle Form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,16 +2,42 @@
 import { TextField, Button } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+/* use this router because it works with appRouter, latest with nextjs */
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+	title: string;
+	description: string;
+}
 
 const NewIssuePage = () => {
+	const router = useRouter();
+	/* destructure props from our useForm */
+	const { register, control, handleSubmit } = useForm<IssueForm>();
 	return (
-		<div className='max-w-xl space-y-3'>
+		<form
+			/* use axios to send our data to the backend API */
+			onSubmit={handleSubmit(async (data) => {
+				await axios.post('/api/issues', data);
+				router.push('/issues');
+			})}
+			className='max-w-xl space-y-3'>
 			<TextField.Root>
-				<TextField.Input placeholder='Title' />
+				{/* destructure register to get all the props */}
+				<TextField.Input placeholder='Title' {...register('title')} />
 			</TextField.Root>
-			<SimpleMDE placeholder='Description' />
+			{/* use Controller because we can't use register destructure as above */}
+			<Controller
+				name='description'
+				control={control}
+				render={({ field }) => (
+					<SimpleMDE placeholder='Description' {...field} />
+				)}
+			/>
 			<Button>Submit New Issue</Button>
-		</div>
+		</form>
 	);
 };
 
